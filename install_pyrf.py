9f84e4c8aa78f9d9ce1779c7608b70070dd6f013
PASQUALE MAINOLFI <pm@MacBook-Pro-PM.local>
2023-10-17 18:49:45 +0200

    add win mode in import pyrf

--- install_pyrf.py
+++ install_pyrf.py
@@ -1,10 +1,13 @@
 # import section
 import subprocess
 import os
+import sys
 
 # main scripts
 CWD = os.getcwd()
-PYFT_PATH = os.path.join(CWD, "pyrustlibfilt/pyft.py")
+OPS = os.name
+_pyft = "pyrustlibfilt/pyft.py" if OPS == "posix" else "pyrustlibfilt\pyft.py"
+PYFT_PATH = os.path.join(CWD, _pyft)
 IMPORT_STRING = "from rustlibfilt.pyrf import Rustlibfilt\n"
 
 # main function
@@ -15,7 +18,8 @@ def main() -> None:
     rustlibfilt_lib_path = os.path.join(site_packages_path, "rustlibfilt")
     init_file_path = os.path.join(rustlibfilt_lib_path, "__init__.py")
     
-    subprocess.run(["cp", PYFT_PATH, rustlibfilt_lib_path])
+    cmd = "cp" if OPS == "posix" else "copy"
+    subprocess.run([cmd, PYFT_PATH, rustlibfilt_lib_path])
     print("[DONE] pyrustlibfilt.pyrf imported")
     
     with open(init_file_path, "r") as file:
@@ -29,7 +33,7 @@ def main() -> None:
         file.write(IMPORT_STRING)
                 
     print("[DONE] __init__.py updated\n")
-
+    
 # [MAIN PROGRAM]: if the module is being run as the main program, it calls the "main()" function
 if __name__ == "__main__":
     main()
